70b880f942430d6e96bb761394a8c49b2f5c8f59
Simon Emanuel Schmid <simon@schmid.io>
2017-07-03 14:54:44 +0200

    inspect env vars & settings

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,5 +1,9 @@
+import { Meteor } from 'meteor/meteor';
+
 // Syncs all the collections according to the blockchain state
 import '/imports/startup/server/sync-collections';
 
 // Helper Method to determine if server is connected
 import '/imports/api/methods/isServerConnected';
+
+console.log('SERVER STARTUP', Meteor.settings, process.env);
