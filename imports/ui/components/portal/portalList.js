ab219d9459e177fdf92b1f610eb7c26a601d631c
Jenna Zenk <zenkjenna@gmail.com>
2017-07-05 19:17:41 +0200

    add sorting arrows on AUM and share price column.

--- imports/ui/components/portal/portalList.js
+++ imports/ui/components/portal/portalList.js
@@ -5,16 +5,33 @@ import Vaults from '/imports/api/vaults';
 
 import './portalList.html';
 
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import store from '/imports/startup/client/store';
+import { creators } from '/imports/redux/user';
+
 Template.portalList.onCreated(() => {
   Meteor.subscribe('vaults');
+  const template = Template.instance();
+  template.aumSort = new ReactiveVar(-1);
+  template.sharePriceSort = new ReactiveVar(-1);
+  store.subscribe(() => {
+    const currentState = store.getState().user;
+    template.aumSort.set(currentState.aumSort);
+    template.sharePriceSort.set(currentState.sharePriceSort);
+  });
 });
 
 Template.portalList.helpers({
-  searchedVaults: () =>
-    Vaults.find(
+  searchedVaults: () => {
+    const aumSort = Template.instance().aumSort.get();
+    const sharePriceSort = Template.instance().sharePriceSort.get();
+    console.log({ aumSort, sharePriceSort });
+    return Vaults.find(
       { name: { $regex: `.*${Session.get('searchVaults')}.*`, $options: 'i' } },
-      { sort: { sharePrice: -1, createdAt: -1 } },
-    ),
+      { sort: { sharePrice: sharePriceSort, nav: aumSort, createdAt: -1 } },
+    );
+  },
   getVaultLink: address =>
     Template.instance().data.main() === 'visit'
       ? `/visit/${address}`
@@ -23,4 +40,17 @@ Template.portalList.helpers({
 
 Template.portalList.onRendered(() => {});
 
-Template.portalList.events({});
+Template.portalList.events({
+  'click .js-AUM-asc': () => {
+    store.dispatch(creators.sortAUM(1));
+  },
+  'click .js-AUM-desc': () => {
+    store.dispatch(creators.sortAUM(-1));
+  },
+  'click .js-sharePrice-asc': () => {
+    store.dispatch(creators.sortSharePrice(1));
+  },
+  'click .js-shareprice-desc': () => {
+    store.dispatch(creators.sortSharePrice(-1));
+  },
+});
