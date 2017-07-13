49a2ab049d9acf75836b6435006aac087f006274
Simon Emanuel Schmid <simon@schmid.io>
2017-07-13 17:45:20 +0200

    add a loading timeout before saying fund not found. closes #317

--- imports/ui/pages/fund.js
+++ imports/ui/pages/fund.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { FlowRouter } from 'meteor/kadira:flow-router';
+import { ReactiveVar } from 'meteor/reactive-var';
 import Vaults from '/imports/api/vaults';
 import '/imports/ui/components/portfolio/portfolioOverview';
 import '/imports/ui/components/portfolio/portfolioContents';
@@ -8,16 +9,29 @@ import '/imports/ui/components/portfolio/manageParticipation';
 import './fund.html';
 
 Template.fund.onCreated(() => {
+  const instance = Template.instance();
   Meteor.subscribe('vaults');
   Meteor.subscribe('assets', FlowRouter.getParam('address'));
-});
 
-Template.fund.helpers({
-  fundExisting() {
+  instance.fundExisting = () => {
     const address = FlowRouter.getParam('address');
     const doc = Vaults.findOne({ address });
     return !(doc === undefined || address === undefined);
+  };
+
+  instance.fundLoadingTimeout = new ReactiveVar(
+    window.setTimeout(() => {
+      instance.fundLoadingTimeout.set(false);
+    }, 10000),
+  );
+});
+
+Template.fund.helpers({
+  loading() {
+    const instance = Template.instance();
+    return !instance.fundExisting() && !!instance.fundLoadingTimeout.get();
   },
+  fundExisting: () => Template.instance().fundExisting(),
   isVisitor: () => Template.instance().data.visit,
 });
 
